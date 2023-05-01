acdcd2d767e064eb3c41d0795b19fb02a6ee28e9
melyasanya <werbolom@gmail.com>
2023-05-01 18:37:47 +0300

    without full 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  console.log(data);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.on('timeupdate', throttle(onPlay, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+// savedValues();
+const inputValues = {};
+
+form.addEventListener('input', throttle(inputMessage, 500));
+
+function inputMessage(event) {
+  inputValues[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputValues));
+}
+
+// function savedValues() {
+//   const savedInput = localStorage.getItem('feedback-form-state');
+//   const parsedSavedInput = JSON.parse(savedInput);
+//   if (parsedSavedInput) {
+//   }
+// }
+
+const submitForm = form.addEventListener('submit', event => {
+  event.preventDefault();
+  localStorage.clear();
+  event.target.reset();
+  console.log(inputValues);
+});
